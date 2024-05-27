62be90346daf6d2ed868bf6206d4fd920b3bb407
Dashanesta <71793564+Dashanesta@users.noreply.github.com>
2024-05-27 15:37:17 -0400

    Added Crate class

---
+++ Crate.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Crate obstacles for the bike to dodge.
+ * 
+ * @author Shane DG
+ * @version May 2024
+ */
+public class Crate extends Actor
+{
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
